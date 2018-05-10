c9b048351258835f708bbd604b04c9fecfea64fd
Patrick Araújo <araujo.patrick@icloud.com>
2018-05-10 11:34:11 -0300

    Update TAP.h

--- TAP.h
+++ TAP.h
@@ -1,29 +1,18 @@
 #ifndef TAP_H_INCLUDED
 #define TAP_H_INCLUDED
 
-typedef enum {
-	Interno, Externo
-}NoTipo;
+typedef struct AP{
+	int bit;
+	char info;
+	struct AP *esq;
+	struct AP *dir;
+} TAP;
 
-typedef struct AP* TAP;
-
-typedef struct AP {
-	NoTipo nt;
-	union {
-		struct {
-			int Index;
-			TAP Esq, Dir;
-		}NInterno;
-		int Chave;
-	}NO;
-}AP;
-
-int Bit(int, int, int);
-int EExterno(TAP);
-TAP CriaNoInt(TAP*,  TAP*, int);
-TAP CriaNoExt(int);
-void Pesquisa(TAP, int, int);
-TAP InsereEntre(TAP*, int, int, int);
-TAP Insere(TAP*, int, int);
+int pegaBit(char, int);
+TAP *insere(TAP*, char, int);
+TAP *inserePatricia(TAP*, char, int);
+TAP *removePatricia(TAP*, char);
+void caminhaEmOrdem(TAP*);
+void imprimeLetraBin(char, int);
 
 #endif // TAP_H_INCLUDED
